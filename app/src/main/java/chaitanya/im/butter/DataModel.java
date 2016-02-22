3dc22f92b815104118dbbf535b4d9bdd5cb2cdac
Chaitanya Nettem <chaitanyanettem@gmail.com>
2016-02-22 14:15:40 +0530

    Implemented recyclerview with gridlayout, imageview and picasso
    Shows a single image in all cards right now.

---
+++ app/src/main/java/chaitanya/im/butter/DataModel.java
@@ -0,0 +1,7 @@
+package chaitanya.im.butter;
+
+/**
+ * Created by arrayjumper on 2/22/16.
+ */
+public class DataModel {
+}
---
+++ app/src/main/java/chaitanya/im/butter/MyAdapter.java
@@ -0,0 +1,7 @@
+package chaitanya.im.butter;
+
+/**
+ * Created by arrayjumper on 2/21/16.
+ */
+public class MyAdapter {
+}
